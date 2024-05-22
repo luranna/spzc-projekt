f47eeed7e3d1d0ffe63cbf0c231203a9b4914176
luranna <ania.luranc@interia.pl>
2024-05-22 20:41:08 +0200

    library cleanup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import opendatasets as od
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import warnings
